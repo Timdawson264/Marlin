3c3c1ac20a1ee52b873dc30730736f1e5665ca24
Dan Collins <dan@collinsnz.com>
2020-02-03 20:16:34 +1300

    Changed Z microsteps, commiting (oldish) eeprom backup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -719,7 +719,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 5120, 160 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1280, 160 }
 
 /**
  * Default Max Feed Rate (mm/s)
